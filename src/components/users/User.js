29ebbb585d37ebc62816043cded7535e661f5505
Emmet <emmetlawton@gmail.com>
2020-03-17 18:59:47 +0000

    changed App.js from a class to a function 25

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,44 +1,35 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-export class User extends Component {
-    componentDidMount() {
-        this.props.getUser(this.props.match.params.login);
-        this.props.getUserRepos(this.props.match.params.login);
-    }
+const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
 
-    static propTypes = {
-        loading: PropTypes.bool,
-        user: PropTypes.object.isRequired,
-        repos: PropTypes.array.isRequired,
-        getUser: PropTypes.func.isRequired,
-        getUserRepos: PropTypes.func.isRequired,
-    }
-
-    render() {
-        const {
-            name,
-            company, 
-            avatar_url,
-            location,
-            bio,
-            blog,
-            login,
-            html_url,
-            followers,
-            following,
-            public_repos,
-            hireable
-        } = this.props.user;
-
-        const { repos, loading } = this.props;
-        console.log(loading);
-        
-        if(loading) return <Spinner/>;
-        return <Fragment>
+    useEffect(() => {
+        getUser(match.params.login);
+        getUserRepos(match.params.login);
+        // eslint-disable-next-line
+    }, []);
+   
+    const {
+        name,
+        company, 
+        avatar_url,
+        location,
+        bio,
+        blog,
+        login,
+        html_url,
+        followers,
+        following,
+        public_repos,
+        hireable
+    } = this.props.user;
+    
+    if(loading) return <Spinner/>;
+    return (
+        <Fragment>
             <Link to='/' className='btn btn-light'>
                 Back to Search
             </Link>
@@ -88,7 +79,15 @@ export class User extends Component {
             <Repos repos={repos}/>
         </Fragment>
              
-    } 
+    )
+}
+
+User.propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
 }
 
 export default User
