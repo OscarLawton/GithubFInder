cf899584c06a32463e567d5295348f4da39e281d
Emmet <emmetlawton@gmail.com>
2020-03-18 20:53:41 +0000

    create reducer action 27

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -22,13 +22,26 @@ const GithubState = props => {
 
     const [state, dispatch] = useReducer(GithubReducer, initialState);
 
+    // Search Github users
+    const searchUsers = async (text) => {
+        setLoading(true);
+        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&cleint_secret=${process.env.REACT_APPC_GITHUB_CLIENT_SECRET}`);
+        dispatch({
+            type: SEARCH_USERS,
+            payload: res.data
+        });
+    }
 
+    // Set Loading 
+    const setLoading = () => dispatch({type: SET_LOADING});
+    
     return <GithubContext.Provider 
         value={{
             users: state.users,
             user: state.user,
             repos: state.repos,
-            loading: state.loading
+            loading: state.loading,
+            searchUsers
         }}
     >
         {props.children}
